3c1e32ac06b5bad35fe8207e5d168c78f5ae46fd
altavarubia-byte <altava.rubia@gmail.com>
2026-05-21 16:31:01 +0200

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,4 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
     },
 
-    sitemap: 'https://mastesto.es/sitemap.xml',
-  }
+    sitemap: 'https://www.mastesto.es/sitemap.xml',
+  };
 }
