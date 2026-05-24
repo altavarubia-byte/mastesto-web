4e831aefd371a69e71cd48cc0bf6cafd1c3f3e7f
altavarubia-byte <altava.rubia@gmail.com>
2026-05-24 19:22:58 +0200

    Update ModelObjeto.tsx

--- components/ModelObjeto.tsx
+++ components/ModelObjeto.tsx
@@ -3,10 +3,19 @@
 import { Clone, useGLTF } from "@react-three/drei";
 
 export default function ModelObjeto({ tipo }: { tipo: string }) {
-  const rutas: Record<string, string> = {
-    router: "/modelos/wifi.glb",
-    receptor: "/modelos/wifi.glb",
-  };
+  const rutas: Record<string,string>={
+
+router:"/modelos/wifi.glb",
+receptor:"/modelos/wifi.glb",
+
+cama:"/modelos/cama.glb",
+sofa:"/modelos/sofa.glb",
+tv:"/modelos/tv.glb",
+armario:"/modelos/armario.glb",
+mesa:"/modelos/mesa.glb",
+silla:"/modelos/silla.glb"
+
+}
 
   const ruta = rutas[tipo];
 
