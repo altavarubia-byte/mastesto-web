1009eb23dd209d98fa66c4785116a8e49448a443
altavarubia-byte <altava.rubia@gmail.com>
2026-05-24 20:38:26 +0200

    Update ModelObjeto.tsx

--- components/ModelObjeto.tsx
+++ components/ModelObjeto.tsx
@@ -1,38 +1,19 @@
 "use client";
 
-import { Clone, useGLTF } from "@react-three/drei";
+import dynamic from "next/dynamic";
 
-export default function ModelObjeto({ tipo }: { tipo: string }) {
-<<<<<<< HEAD
+const Modelo3D = dynamic(
+  () => import("./ModelObjetoCliente"),
+  {
+    ssr: false,
+    loading: () => null,
+  }
+);
 
-  const rutas: Record<string,string> = {
-    router:"/modelos/wifi.glb",
-    receptor:"/modelos/wifi.glb",
-  };
-=======
-  const rutas: Record<string,string>={
-
-router:"/modelos/wifi.glb",
-receptor:"/modelos/wifi.glb",
-
+export default function ModelObjeto({
+  tipo,
+}: {
+  tipo: string;
+}) {
+  return <Modelo3D tipo={tipo} />;
 }
->>>>>>> 9e785fec3a8a43a0a2e8cd65160049c57fe5bfd9
-
-  const ruta = rutas[tipo];
-
-  if(!ruta) return null;
-
-  const gltf = useGLTF(ruta);
-
-<<<<<<< HEAD
-  return (
-    <Clone
-      object={gltf.scene}
-      scale={0.8}
-    />
-  );
-}
-=======
-  return <Clone object={gltf.scene} scale={0.8} />;
-}
->>>>>>> 9e785fec3a8a43a0a2e8cd65160049c57fe5bfd9
