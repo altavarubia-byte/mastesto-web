c07f861e3d924865a8bd2eb8c4b8fcaaf8656bd9
Vicente Altava <altava.rubia@gmail.com>
2026-05-24 20:28:53 +0200

    Cambios locales antes de sincronizar

--- components/ModelObjeto.tsx
+++ components/ModelObjeto.tsx
@@ -3,16 +3,22 @@
 import { Clone, useGLTF } from "@react-three/drei";
 
 export default function ModelObjeto({ tipo }: { tipo: string }) {
-  const rutas: Record<string, string> = {
-    router: "/modelos/wifi.glb",
-    receptor: "/modelos/wifi.glb",
+
+  const rutas: Record<string,string> = {
+    router:"/modelos/wifi.glb",
+    receptor:"/modelos/wifi.glb",
   };
 
   const ruta = rutas[tipo];
 
-  if (!ruta) return null;
+  if(!ruta) return null;
 
   const gltf = useGLTF(ruta);
 
-  return <Clone object={gltf.scene} scale={0.8} />;
+  return (
+    <Clone
+      object={gltf.scene}
+      scale={0.8}
+    />
+  );
 }
